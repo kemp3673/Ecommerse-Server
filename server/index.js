eb463d4b904431ec76f97e3512aabfcd7fcdaf11
Nicholas Kempkes <nicholas.kempkes@gmail.com>
2022-08-01 08:39:01 -0500

    Added CORS middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const newrelic = require('newrelic');
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const queries = require("../postgreSQL/db.js");
 const controllers = require('./controllers.js');
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
